3129243d5f5c9889a4349d93811b41b062513946
senakaracabey34-lgtm <senakaracabey34@gmail.com>
2026-06-02 21:39:33 +0300

    Add files via upload

---
+++ static/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "riskatlas-cache-v1";
+
+const FILES_TO_CACHE = [
+    "/",
+    "/static/manifest.json",
+    "/static/icon.png",
+    "/static/riskatlas-bg.png",
+    "/static/mobile-bg.png"
+];
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request);
+        })
+    );
+});
